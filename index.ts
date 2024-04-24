6118d9f40391b05ad464fc2b9b4eaed425ac719a
Hooria Osama (Hina) <159541875+hinaosama1@users.noreply.github.com>
2024-04-24 19:58:08 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+import chalk from "chalk";
+
+const RandomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([{
+name: "userguessnumber",
+type: "number",
+message: "please guess a number between 1-10: ",
+
+}]);
+
+if (answers.userguessnumber === RandomNumber){
+    console.log(chalk.blue(`congratulations! you guessed right number`));
+}else{
+  console.log(chalk.yellow(`you guessed wrong number`));
+};
